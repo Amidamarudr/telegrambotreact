fb587ddd337acd416245926bea1cbf06177ba589
amidamarudr <dev.amidamaru@mail.ru>
2023-05-28 18:53:26 +0300

    add import om Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../Button/Button';
 import { useTelegram } from '../../hooks/useTelegram';
 
 const Header = () => {
@@ -7,7 +8,9 @@ const Header = () => {
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>{user?.username}</span>
+            <span className={'username'}>
+                {user?.username}
+            </span>
         </div>
   )
 }
